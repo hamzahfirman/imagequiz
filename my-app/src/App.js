6eae057a8ed0024a65015bea18368749c7b95d20
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-10-05 12:35:04 -0700

    New Push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//Components
+import Home from './components/home-page';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to Image Quiz App!</h1>
-        <form action="/components/home-page">
-          <label for="fname">Username</label> &nbsp;
-          <input type="text" id="fname" name="fname"/> &nbsp;
-          <label for="lname">Password</label> &nbsp;
-          <input type="password" id="lname" name="lname"/><br/><br/>
-          <input type="submit" value="Log In" />
-      </form> 
-      <button id="signUp">Sign Up</button>
-      </header>
+        <Home />
+        </header>
     </div>
   );
 }
--- my-app/src/components/home-page.js
+++ my-app/src/components/home-page.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-
 class HomePage extends React.Component {
   render() {
-      return (
-        <div className="Content-Page">
-        <header className="Page-header">
-             <h1>This is the Home Page</h1>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Image Quiz App!</h1>
+          <form action="/components/home-page">
+            <label for="fname">Username</label> &nbsp;
+            <input type="text" id="fname" name="fname"/> &nbsp;
+            <label for="lname">Password</label> &nbsp;
+            <input type="password" id="lname" name="lname"/><br/><br/>
+            <input type="submit" value="Log In" />
+        </form> 
+        <button id="signUp">Sign Up</button>
         </header>
-        </div>
-  );
+      </div>
+    );
 }};
 
 export default HomePage;
