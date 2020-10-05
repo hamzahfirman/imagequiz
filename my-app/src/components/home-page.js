2ee74670dd79a75085a9a02136197625d27a8f67
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-10-05 15:30:22 -0700

    New Push

--- my-app/src/components/home-page.js
+++ my-app/src/components/home-page.js
@@ -6,43 +6,69 @@ class HomePage extends React.Component {
     super(props);
     // State is an object
     this.state = {
-        showLoginForm: false
+        username: '',
+        showLoginForm: false,
+        authenticated: false
     };
   }
-
   // Events
   login = () => {
     this.setState({
       showLoginForm: true
     });
   }
-  signUp = () => {
+  // Submit 
+  logInSubmit = (event) => {
+    if(this.state.username.trim().length > 0){
+      //Let them into the Homepage
+      this.setState({
+        authenticated: true
+      });
+    }
+    event.preventDefault();
 
-
+  }
+  hadleChange= (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    // Changes the username
+    this.setState({
+      [name]: value
+    });
   }
   // Render function: What user see on'Homepage'
   render() {
+
     // CONDITION: If the login button gets clicked
-    if(this.state.showLoginForm){
+    if(!(this.state.authenticated) && this.state.showLoginForm){
       return(
           <div>
-            <form>
-              <label for="fname">Username</label> &nbsp;
-              <input type="text" id="fname" name="fname"/> &nbsp;
-              <label for="lname">Password</label> &nbsp;
-              <input type="password" id="lname" name="lname"/><br/><br/>
-              <button id="goLogIn">Go</button>
+            <form onSubmit={this.logInSubmit}>
+              <label for="username">Username</label> &nbsp;
+              <input type="text" id="username" 
+              value={this.state.username} 
+              onChange={this.hadleChange} 
+              name="username"/> &nbsp;
+              <label for="password">Password</label> &nbsp;
+              <input type="password" id="password" name="password"/><br/><br/>
+              <button type="submit" id="loginNow">Login</button>
             </form>
           </div>
       );
 
+    }else if(this.state.authenticated && this.state.showLoginForm){
+        return (
+          <div>
+            Hello World
+          </div>
+        );
     }else{
       return (
         <div className="Home">
           <header className="App-header">
-          <h1>Welcome to Image Quiz App!</h1>
-          <button id="login" onClick={this.login}>Log in</button>
-          <button id="signUp" onClick={this.signUp}>Sign Up</button>
+            <h1>Welcome to Image Quiz App!</h1>
+            <button id="login" onClick={this.login}>Log in</button>
+            <button id="signUp" onClick={this.signUp}>Sign Up</button>
           </header>
         </div>
       );
@@ -51,9 +77,3 @@ class HomePage extends React.Component {
 };
 
 export default HomePage;
-
-// <label for="fname">Username</label> &nbsp;
-// <input type="text" id="fname" name="fname"/> &nbsp;
-// <label for="lname">Password</label> &nbsp;
-// <input type="password" id="lname" name="lname"/><br/><br/>
-// <input type="submit" value="Log In" />
