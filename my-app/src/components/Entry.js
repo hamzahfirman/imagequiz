bef473dcc62ec3d138d1e9e138220b05a2c230de
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-11-05 11:57:37 -0700

    New Push

--- my-app/src/components/Entry.js
+++ my-app/src/components/Entry.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Entry.css';
-// All the entries from the server 
 
+// All necessary data to each quiz question
 var data = [
     {passed: false},
     {passed: false},
     {passed: false},
     {passed: false},
     {passed: false},
-    {passed: false}];
+    {passed: false}
+];
 
 class Entry extends React.Component {
     constructor(props){
@@ -17,6 +18,7 @@ class Entry extends React.Component {
             userAnswers: []
     }
     }
+    // 
     shuffle = (array) => {
         array.sort(() => Math.random() - 0.5);
     }
@@ -29,9 +31,10 @@ class Entry extends React.Component {
         this.setState({userAnswers: data});
         this.props.onChoiceSelected(e.target.value);
     }
-    randomizeAnswersLocation = () => {
+    getPossibleAnswers = () => {
         
         const { entry, cursor } = this.props;
+
         if(data[cursor]["passed"] === false) {   
         
             var rand1 = Math.floor(Math.random() * 6) + 1;
@@ -42,9 +45,10 @@ class Entry extends React.Component {
             let index = this.props.cursor;
             let obj = data[index];
             obj["choices"] = answers;
+
             //To show that this page has been passed 
             data[cursor]["passed"] = true;
-
+            // Randomized the option locations 
             this.shuffle(answers);
             return(
                 <div>
@@ -54,21 +58,20 @@ class Entry extends React.Component {
                 </div>
             );
         }else{
+            // Get all the stored choices 
+            let index = this.props.cursor;
+            let obj = data[index];
+            let storedChoices =  obj["choices"];
+            let currAnswer = obj["answer"];
             return(
                 <div>
-                   Passed!
+                    <input type="radio" name="categories" className="answers" onClick={this.handleOnClick} checked={currAnswer === storedChoices[0] ? "checked" : ""} value={storedChoices[0]}/> {storedChoices[0]}
+                    <input type="radio" name="categories" className="answers" onClick={this.handleOnClick} checked={currAnswer === storedChoices[1]? "checked" : ""} value={storedChoices[1]}/>{storedChoices[1]}
+                    <input type="radio" name="categories" className="answers" onClick={this.handleOnClick} checked={currAnswer === storedChoices[2] ? "checked" : ""} value={storedChoices[2]}/> {storedChoices[2]}
                 </div>
             );
         }
-        // } 
-        // var randomized = this.getRadios(answers);
-        // return(
-        //     <div>
-        //         <input type="radio" name="categories" className="answers" onClick={this.handleOnClick} checked={this.state.userAnswers[cursor] === answers[0] ? "checked" : ""} value={answers[0]}/> {answers[0]}
-        //         <input type="radio" name="categories" className="answers" onClick={this.handleOnClick} checked={this.state.userAnswers[cursor] === answers[1] ? "checked" : ""} value={answers[1]}/>{answers[1]}
-        //         <input type="radio" name="categories" className="answers" onClick={this.handleOnClick} checked={this.state.userAnswers[cursor] === answers[2] ? "checked" : ""} value={answers[2]}/> {answers[2]}
-        //     </div>
-        // );
+
     }
 
     render() {
@@ -85,7 +88,7 @@ class Entry extends React.Component {
                     <p id="question">
                     <div id="image"><img src={require("../images/" + entry.image)} alt={entry.image}/></div>
                     {entry.question}</p>
-                    {this.randomizeAnswersLocation()}
+                    {this.getPossibleAnswers()}
                 </div>
                 
             </div>
